f8a3df39f449e4219b6a5bd60339e5385d7bde71
PedroHFG <pedrohfidg@gmail.com>
2024-11-15 12:49:00 -0300

    Saving the OrderService arquive

--- src/main/java/com/devsuperior/desafio1/services/OrderService.java
+++ src/main/java/com/devsuperior/desafio1/services/OrderService.java
@@ -1,5 +1,19 @@
 package com.devsuperior.desafio1.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.desafio1.entities.Order;
+
+@Service
 public class OrderService {
+	
+	@Autowired
+	private ShippingService shippingService;
+	
+	public double total(Order order) {
+		double discount = order.getBasic() * order.getDiscount() / 100.0;
+		return order.getBasic() - discount + shippingService.shipment(order);
+	}
 
 }
